b0f764d3ebb90768aed6d5cf277f75a771388966
Tamar-Shachar <4134447@gmail.com>
2023-07-25 10:09:00 +0300

    add column in table technician

--- db_creation.py
+++ db_creation.py
@@ -55,14 +55,17 @@ if __name__ == '__main__':
     #         r" FOREIGN KEY (source) REFERENCES device(id)," \
     #         r" FOREIGN KEY (destination) REFERENCES device(id))"
 
-    create_table(query)
+    # create_table(query)
 
-    query_to_insert = "INSERT INTO network (client_id, premises, date) values (1,'Tel Aviv', '2020-03-07')"
+    # query_to_insert = "INSERT INTO network (client_id, premises, date) values (1,'Tel Aviv', '2020-03-07')"
     # query_to_insert = 'INSERT into technician (name) values ("Dan")'
     # query_to_insert = "INSERT INTO device (MAC_address, vendor, network_id) values ('84-A9-3E-AE-80-9B'," \
     #                   " 'Hewlett Packard', 1)"
     # query_to_insert = "INSERT INTO technician_clients (technician_id, client_id) values (1, 1)"
     #query_to_insert = "INSERT INTO device_connection (network_id, source, destination, protocol) values (1, 1, 2, 'UDP')"
-    insert_to_table(query_to_insert)
+    # query_to_update = "UPDATE technician SET email = 'dantech@gmail.com' ,password = 'passwors1234' WHERE id = 1"
+    # query_to_update = "ALTER TABLE technician ADD COLUMN email VARCHAR(255) AFTER name,ADD COLUMN password VARCHAR(255) AFTER email"
+    query_to_change = "ALTER TABLE technician CHANGE password password VARCHAR(255) NOT NULL"
+    insert_to_table(query_to_change)
 
-    get_all_from_table("device_connection")
+    get_all_from_table("technician")
