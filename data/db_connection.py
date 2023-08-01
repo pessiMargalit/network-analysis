3fe7dc30f617d8f9feeadf92acf9831f6dba0be8
pessiMargalit <pessimargalit@gmail.com>
2023-08-01 13:19:45 +0300

    Update the connection to new DB

--- data/db_connection.py
+++ data/db_connection.py
@@ -19,11 +19,11 @@ def disconnect():
 def connect_to_db():
     dbServerName = "sql6.freesqldatabase.com"
 
-    dbUser = "sql6635072"
+    dbUser = "sql6633743"
 
-    dbPassword = "tS6Z6mXSx1"
+    dbPassword = "sql6636743"
 
-    dbName = "sql6635072"
+    dbName = "sql6633743"
 
     charSet = "utf8mb4"
 
